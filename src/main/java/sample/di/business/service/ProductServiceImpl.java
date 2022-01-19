fd5f948238794350319bdcabca0f8bba4ae4a4b7
tomdanbi <bohyun611.kim@gmail.com>
2022-01-19 15:18:45 +0900

    생성자  di

--- src/main/java/sample/di/business/service/ProductServiceImpl.java
+++ src/main/java/sample/di/business/service/ProductServiceImpl.java
@@ -9,11 +9,12 @@ public class ProductServiceImpl implements ProductService {
 
   private ProductDao productDao;
 
+  public ProductServiceImpl() {}
+
   @Autowired
-  public void setProductDao(ProductDao productDao) {
+  public ProductServiceImpl(ProductDao productDao) {
     this.productDao = productDao;
   }
-
 
   @Override
   public void addProduct(Product product) {
